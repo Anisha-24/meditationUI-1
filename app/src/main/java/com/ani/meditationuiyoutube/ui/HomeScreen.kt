5b78d630113ebb45e3727933adac8b27f42df0b5
Anisha <anisha242002@gmail.com>
2022-04-05 17:30:16 +0530

    UI implementation started

--- app/src/main/java/com/ani/meditationuiyoutube/ui/HomeScreen.kt
+++ app/src/main/java/com/ani/meditationuiyoutube/ui/HomeScreen.kt
@@ -1,11 +1,26 @@
 package com.ani.meditationuiyoutube.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.ani.meditationuiyoutube.ui.theme.ButtonBlue
+import com.ani.meditationuiyoutube.ui.theme.DarkerButtonBlue
 import com.ani.meditationuiyoutube.ui.theme.DeepBlue
 
 @Composable
@@ -15,8 +30,68 @@ fun HomeScreen()
         .background(DeepBlue)
         .fillMaxSize()
     ){
-        Column() {
-            
+        Column {
+            GreetingSection()
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Anisha"
+){
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(15.dp)
+        ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good morning, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "Nice to see you $name.",
+                style = MaterialTheme.typography.body2
+            )
+            Text(
+                text = "Have a good day!",
+                style = MaterialTheme.typography.body2
+            )
+        }
+//        Icon(painter = painterResource(id = R.drawable.ic_search),
+//            contentDescription = "Search",
+//            tint = Color.White,
+//            modifier = Modifier.size(24.dp)
+//        )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+){
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow{
+        items(chips.size){
+            Box(modifier = Modifier
+                .padding(start = 15.dp,top = 15.dp,end = 15.dp,bottom = 15.dp)
+                .clickable {
+                    selectedChipIndex = it
+                }
+                .clip(RoundedCornerShape(10.dp))
+                .background(
+                    if(selectedChipIndex == it) ButtonBlue
+                else
+                    DarkerButtonBlue
+                )
+            )
         }
     }
 }
