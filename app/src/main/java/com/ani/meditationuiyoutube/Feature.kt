7f581f69b046b936107903faf25f2990f78c6404
Anisha <anisha242002@gmail.com>
2022-04-06 17:50:02 +0530

    homeScreen on process

--- app/src/main/java/com/ani/meditationuiyoutube/Feature.kt
+++ app/src/main/java/com/ani/meditationuiyoutube/Feature.kt
@@ -1,12 +1,12 @@
 package com.ani.meditationuiyoutube
 
-//import androidx.annotation.DrawableRes
-//import androidx.compose.ui.graphics.Color
-//
-//data class Feature(
-//    val title: String,
-//    @DrawableRes val iconId: Int,
-//    val lightColor: Color,
-//    val mediumColor: Color,
-//    val darkColor: Color
-//)
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+
+data class Feature(
+    val title: String,
+    @DrawableRes val iconId: Int,
+    val lightColor: Color,
+    val mediumColor: Color,
+    val darkColor: Color
+)
--- app/src/main/java/com/ani/meditationuiyoutube/ui/HomeScreen.kt
+++ app/src/main/java/com/ani/meditationuiyoutube/ui/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.ani.meditationuiyoutube.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -12,36 +16,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.ani.meditationuiyoutube.ui.theme.ButtonBlue
-import com.ani.meditationuiyoutube.ui.theme.DarkerButtonBlue
-import com.ani.meditationuiyoutube.ui.theme.DeepBlue
-import com.plcoding.meditationuiyoutube.R
-
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.clickable
-//import androidx.compose.foundation.layout.*
-//import androidx.compose.foundation.lazy.LazyRow
-//import androidx.compose.foundation.shape.RoundedCornerShape
-//import androidx.compose.material.Icon
-//import androidx.compose.material.MaterialTheme
-//import androidx.compose.material.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.remember
-//import androidx.compose.runtime.setValue
-//import androidx.compose.runtime.getValue
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.draw.clip
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.res.painterResource
-//import androidx.compose.ui.unit.dp
-//import com.ani.meditationuiyoutube.ui.theme.ButtonBlue
-//import com.ani.meditationuiyoutube.ui.theme.DarkerButtonBlue
-//import com.ani.meditationuiyoutube.ui.theme.DeepBlue
+import com.ani.meditationuiyoutube.Feature
+import com.ani.meditationuiyoutube.R
+import com.ani.meditationuiyoutube.ui.theme.*
 
 @Composable
 fun HomeScreen()
@@ -52,6 +34,8 @@ fun HomeScreen()
     ){
         Column {
             GreetingSection()
+            ChipSection(chips = listOf("Sweet Sleep", "Insomnia","Depression"))
+            CurrentMeditation()
         }
     }
 }
@@ -100,21 +84,142 @@ fun ChipSection(
     }
     LazyRow{
         items(chips.size){
-            Box(modifier = Modifier
-                .padding(start = 15.dp,top = 15.dp,end = 15.dp,bottom = 15.dp)
-                .clickable {
-                    selectedChipIndex = it
-                }
-                .clip(RoundedCornerShape(10.dp))
-                .background(
-                    if(selectedChipIndex == it) ButtonBlue
-                else
-                    DarkerButtonBlue
-                )
-            )
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, end = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue
+                        else
+                            DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            ){
+                Text(text = chips[it], color = TextWhite)
+            }
         }
     }
 }
+
+@Composable
+fun CurrentMeditation(
+    color: Color = LightRed
+){
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(color)
+            .padding(horizontal = 15.dp, vertical = 20.dp)
+            .fillMaxWidth()
+    ){
+        Text(
+            text = "Daily Thought",
+            style = MaterialTheme.typography.h2
+        )
+        Text(
+            text = "Meditation . 3-10 min",
+            style = MaterialTheme.typography.body2,
+            color = TextWhite
+        )
+    }
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .background(ButtonBlue)
+            .padding(10.dp)
+    ){
+       Icon(
+           painter = painterResource(id = R.drawable.ic_play),
+           contentDescription = "Play",
+           tint = Color.White,
+           modifier = Modifier.size(16.dp)
+       )
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun FeatureSection(features: List<Feature>){
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Features",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(15.dp)
+        )
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+            modifier = Modifier.fillMaxWidth()
+        ){
+            items(features.size){
+
+            }
+        }
+    }
+}
+
+@Composable
+fun FeatureItem(
+    feature: Feature
+){
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        //medium colored path
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+        val mediumColoredPath = Path().apply {
+            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //import androidx.compose.foundation.Canvas
 //import androidx.compose.foundation.ExperimentalFoundationApi
