df75fc792886c06ef71e7c25ace7334750df5bca
Anisha-24 <anisha242002@gmail.com>
2022-04-06 16:06:01 +0530

    minor changes

--- app/src/main/java/com/ani/meditationuiyoutube/MainActivity.kt
+++ app/src/main/java/com/ani/meditationuiyoutube/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.ani.meditationuiyoutube
 
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.compose.foundation.ExperimentalFoundationApi
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.tooling.preview.Preview
-//import com.ani.meditationuiyoutube.ui.HomeScreen
-//import com.ani.meditationuiyoutube.ui.theme.MeditationUIYouTubeTheme
-//
-//class MainActivity : ComponentActivity() {
-//    @ExperimentalFoundationApi
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            MeditationUIYouTubeTheme {
-//                HomeScreen()
-//            }
-//        }
-//    }
-//}
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ani.meditationuiyoutube.ui.HomeScreen
+import com.ani.meditationuiyoutube.ui.theme.MeditationUIYouTubeTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUIYouTubeTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
